c69f68a676f81a54bc7b7be60af0a5c498ed39ad
eye <boonyaporn.sudjunham@kkumail.com>
2019-12-26 15:08:21 +0700

    login/register

--- build/js/authenSelect.js
+++ build/js/authenSelect.js
@@ -1,22 +1,9 @@
-var colSelrole = document.querySelector(".login");
-var colOwnerLogin = document.querySelector(".owner-login");
-var colOwnerRegis = document.querySelector(".owner-register");
-var colOwnerRegisGG = document.querySelector(".owner-gg-register");
-var colManagerLogin = document.querySelector(".manager-login");
-var colManagerRegis = document.querySelector(".manager-regis");
+var ownerRoleBt = document.getElementById("owner-sel-role-bt");
+var managerRoleBt = document.getElementById("manager-sel-role-bt");
+var ownerLoginUI = document.getElementById("owner-login-ui");
+var managerLoginUI = document.getElementById("manager-login-ui");
+checkHash();
 
-// function setLabelAuthen(className) {
-//   document.querySelector("." + className).style.display = "block";
-//   colManagerLogin.style.display = "none";
-// }
-
-$(document).scroll(function() {
-  windowScroll();
-});
-
-$(window, document).ready(function() {
-  checkHash();
-});
 
 $(window).bind("hashchange", function() {
   checkHash();
@@ -26,70 +13,130 @@ function checkHash() {
   var href = window.location.hash;
   switch (href) {
     case "": {
-      setLabelAuthen(colSelrole);
+      sessionStorage.role = "owner";
       break;
     }
-    case "#owner-login": {
-      setLabelAuthen(colOwnerLogin);
+    case "#role-owner": {
+      sessionStorage.role = "owner";
       break;
     }
-    case "#manager-login": {
-      setLabelAuthen(colManagerLogin);
-      break;
-    }
-    case "#owner-rergis": {
-      setLabelAuthen(colOwnerRegis);
-      break;
-    }
-    case "#owner-authen": {
-      //check db register
-      window.location = "MainPage.html";
-      break;
-    }
-    case "#owner-rergis-gg": {
-      //check db register
-      window.location = "MainPage.html";
-      break;
-    }
-    case "#owner-rergis-success": {
-      setLabelAuthen(colOwnerLogin);
-      break;
-    }
-    case "#manager-rergis": {
-      setLabelAuthen(colManagerRegis);
-      break;
-    }
-    case "#manager-authen": {
-      window.location = "MainPage.html";
-      break;
-    }
-    case "#manager-regis-success": {
-      setLabelAuthen(colManagerLogin);
+    case "#role-manager": {
+      sessionStorage.role = "manager";
       break;
     }
   }
+  selLoginUI();
 }
 
-function setLabelAuthen(className) {
-  var labelArr = [
-    colSelrole,
-    colOwnerLogin,
-    colOwnerRegis,
-    colOwnerRegisGG,
-    colManagerLogin,
-    colManagerRegis
-  ];
-  for (let i = 0; i < labelArr.length; i++) {
-    if (className == labelArr[i]) {
-      className.style.display = "block";
+function selLoginUI() {
+  var role = sessionStorage.role || undefined;
+  if (role != undefined) {
+    if (role == "owner") {
+      $("#owner-login-ui").fadeIn();
+      managerLoginUI.style.display = "none";
+      managerRoleBt.style.backgroundColor = "white";
+      ownerRoleBt.style.backgroundColor = "rgb(53, 173, 87)";
+      $("#text-manager-role").css("color", "black");
+      $("#text-owner-role").css("color", "white");
     } else {
-      labelArr[i].style.display = "none";
+      $("#manager-login-ui").fadeIn();
+      ownerLoginUI.style.display = "none";
+      ownerRoleBt.style.backgroundColor = "white";
+      managerRoleBt.style.backgroundColor = "rgb(53, 173, 87)";
+      $("#text-owner-role").css("color", "black");
+      $("#text-manager-role").css("color", "white");
     }
   }
 }
 
-function windowScroll() {
-  var st = $(document).scrollTop();
-
-  $("#bg-mobile").css({ top: 0 + st * 1 + "px" });
+//phone login
+function prepPhoneAuth() {
+  window.phoneCheck = false;
+  window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+    "sign-in-button",
+    {
+      size: "invisible",
+      callback: function(response) {
+        firebaseAuthenByPhoneNumber();
+      }
+    }
+  );
+  recaptchaVerifier.render().then(function(widgetId) {
+    window.recaptchaWidgetId = widgetId;
+  });
 }
+function firebaseAuthenByPhoneNumber() {
+  if (window.phoneCheck) {
+    window.singingIn = true;
+    var phoneNumber = getPhoneNumberFromUserInput();
+    var appVerifier = window.recaptchaVerifier;
+    firebase
+      .auth()
+      .signInWithPhoneNumber(phoneNumber, appVerifier)
+      .then(function(confirmationResult) {
+        window.confirmationResult = confirmationResult;
+        window.singingIn = false;
+        $("#manager-verify-ui").fadeIn();
+        document.getElementById("manager-login-form").style.display = "none";
+      })
+      .catch(function(error) {
+        console.log("error durong signInWithPhoneNumber");
+        window.alert(
+          "error durong signInWithPhoneNumber:\n\n" +
+            error.code +
+            "\n\n" +
+            error.message
+        );
+        window.signingIn = false;
+      });
+  }
+}
+
+function onVerifyCodeSubmit(e) {
+  e.preventDefault();
+  if (!!getCodeFromUserInput()) {
+    window.verifyingCode = true;
+    var code = getCodeFromUserInput();
+    confirmationResult
+      .confirm(code)
+      .then(function(result) {
+        var user = result.user;
+        window.verifyingCode = false;
+      })
+      .catch(function(error) {
+        console.error("error while chacking verification code", error);
+        window.alert(
+          "error while chacking verification code: \n\n" +
+            error.code +
+            "\n\n" +
+            error.message
+        );
+        window.verifyingCode = false;
+      });
+  }
+}
+
+function cancelVerification(e) {
+  e.preventDefault();
+  window.confirmationResult = null;
+}
+
+function isPhoneNumberValid() {
+  var pattern = /^\+[0-9\s\-\(\)]+$/;
+  var phoneNumber = document.getElementById("phone-manager-input").value;
+  phoneNumber = phoneNumber.replace(/\s/g, "");
+  phoneNumber = phoneNumber.replace(/-/g, "");
+  var formatPhone = "+66" + phoneNumber.slice(1, 10);
+  window.phoneCheck = true;
+}
+
+function resetReCaptcha() {
+  if (
+    typeof grecaptcha !== "undefined" &&
+    typeof window.recaptchaWidgetId !== "undefined"
+  ) {
+    grecaptcha.reset(window.recaptchaWidgetId);
+  }
+}
+
+//------------------------------------
