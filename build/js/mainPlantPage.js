1976af318b49b7f16afb3d452153f2ae88bddcd0
aiboonyapon <boonyapon.m@kkumail.com>
2020-01-25 17:16:59 +0700

    some plant page

--- build/js/mainPlantPage.js
+++ build/js/mainPlantPage.js
@@ -2,7 +2,7 @@ var plantArray = []
 
 window.onload = function() {
   var allPlants = JSON.parse(localStorage["plants"]);
-  var plantCard = document.getElementById("plantCard");
+  var plantCard = document.getElementById("plant-div");
   for(var i = 0; i < allPlants.length; i++){
     managerArray = allPlants[i].name
 
@@ -23,3 +23,24 @@ window.onload = function() {
   }
 }
 
+var collectedPlantName = document.getElementById("plantName").value
+var collectedPlantPicture = document.getElementById("plantPicUpload").value
+
+function postNewPlants() {
+console.log("ownerId" , ownerId)
+  var url = "/plants/" + ownerId
+  var body = JSON.stringify({
+    name: collectedPlantName,
+    cover_image: "0000"
+  });
+  var postNewPlants = connectToServer(url, body, "POST");
+  postNewPlants.then(
+    docs => {
+      console.log("create new plant success " + docs)
+    },
+    function (e) {
+      // 404 owner not found
+      console.log(e.responseText);
+    }
+  );
+}
--- build/js/navBarLinks.js
+++ build/js/navBarLinks.js
@@ -15,6 +15,8 @@ id="navBarPlant"*/
   reportLink.href = "reportPerLand.html"
   var managerLink = document.getElementById("navBarManager")
   managerLink.href = "managers.html?id=123"
+  var plantLink = document.getElementById("navBarPlant")
+  plantLink.href = "mainPlantPage.html"
 
 
 
