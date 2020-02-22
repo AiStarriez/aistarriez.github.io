ec3a3788a3658b02a6580bd980d9a3b68e26905e
aiboonyapon <boonyapon.m@kkumail.com>
2020-02-22 18:14:28 +0700

    fix weather forecast show

--- public/js/callLandDetailInfo.js
+++ public/js/callLandDetailInfo.js
@@ -9,21 +9,23 @@ var plantId;
 var hash = window.location.hash;
 hash = hash.replace("#", "");
 
-function getallLands() {
+
+run();
+async function getallLands() {
   var allLands = JSON.parse(localStorage["lands"]);
   currentLand = allLands.find(x => x.land._id === hash);
   // console.log(hash);
   console.log(currentLand.operation.logs.plant_id);
 }
 
-function getallPlants() {
+async function getallPlants() {
   var allPlants = JSON.parse(localStorage["plants"]);
   currentPlantId = currentLand.operation.logs.plant_id;
   currentPlant = allPlants.find(x => x._id === currentPlantId);
   console.log(currentPlant);
 }
 
-function callOpCyCle() {
+async function callOpCyCle() {
   var url = "/operations/" + hash;
   var body = "";
   var getOpCycle = connectToServer(url, body, "GET");
@@ -68,10 +70,16 @@ function displayActivity() {
   $("#showLateActivity").append(lateActivity);
 }
 
-getallLands();
-getallPlants();
-callOpCyCle();
-displayActivity();
+
+
+
+async function run() {
+
+  await getallLands();
+  await getallPlants();
+  await callOpCyCle();
+  displayActivity();
+  
 //console.log(currentLandInfo.land.name)
 var landNameArr = document.querySelectorAll("#landNameTitle");
 for (i in landNameArr) {
@@ -82,6 +90,8 @@ $("#landName").append(currentLand.land.name);
 $("#plant").append(currentPlant.name);
 $("#expProduct").append(currentLand.operation.logs.expected_product);
 //console.log(currentOpCycle);
+
+
 $("#save-harvested-product").click(async () => {
   var harvestedDate = document.getElementById("harvested-date");
   var realProduct = document.getElementById("real-product");
@@ -93,3 +103,4 @@ $("#save-harvested-product").click(async () => {
     $("#err-div-modal").show();
   }
 });
+}
